152597f8a79637e436f01f77c479316789b63b19
rhask <brianjohncarbone@gmail.com>
2022-02-28 23:06:40 -0500

    add text

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,8 +1,17 @@
-export const onRequestGet: PagesFunction = async ({ request, next, env }) => {
-  const { cf, url } = request;
+export const onRequestGet: PagesFunction = async ({ request, next }) => {
+  const { cf } = request;
   const { city, regionCode, country } = cf;
   const response = await next();
-  return new HTMLRewriter().on('span.country', { 
-    element(element) { element.setInnerContent(`${city}, ${regionCode}, ${country}`) } 
-  }).transform(response);
+
+  const rewriters = [
+    new HTMLRewriter().on('span.country', {
+      element(element) { element.setInnerContent(`${city}, ${regionCode}, ${country}`) }
+    }),
+
+    new HTMLRewriter().on('#regulations .tick', {
+      element(element) { element.setInnerContent(`Regulatory compliance alerts for ${country} available. Access alerts ➞`) }
+    })
+  ];
+
+  return rewriters.reduce((prev, curr) => curr.transform(prev), response);
 };
