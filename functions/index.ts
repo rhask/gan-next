ba7f591cc743b3c45f899a48bce92d105d6dfe7d
rhask <brianjohncarbone@gmail.com>
2022-05-05 15:03:42 -0400

    okay

--- functions/index.ts
+++ functions/index.ts
@@ -1,10 +1,19 @@
+class ClearbitHandler implements HTMLRewriterElementContentHandlers {
+  async element(element: Element): Promise<void> {
+    setTimeout(() => 3000);
+    element.setInnerContent("holy hell");
+  }
+}
+
 export const onRequestGet: PagesFunction = async ({ request, next }) => {
   const { cf } = request;
   const { city, regionCode, country } = cf;
-  const response = await next();
 
-  return new HTMLRewriter()
+  const response = new HTMLRewriter()
     .on("span.country", { element(el) { el.setInnerContent(`${city}, ${regionCode}, ${country}`) } })
     .on("#regulations .tick", { element(el) { el.setInnerContent(`Regulatory compliance alerts for ${country} available. Access alerts ➞`) } })
-    .transform(response);
+    .on("#test", new ClearbitHandler())
+    .transform(await next());
+
+  return response;
 };
