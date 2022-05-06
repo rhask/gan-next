3d74a0071c4d0a7d11c142a5d4ec3659cd96e256
rhask <brianjohncarbone@gmail.com>
2022-05-06 17:12:20 -0400

    ok

--- functions/index.ts
+++ functions/index.ts
@@ -1,4 +1,4 @@
-export const onRequestGet: PagesFunction<{ CBSK: string, LFSK: string }> = async ({ request, next, env }) => {
+export const onRequestGet: PagesFunction<{ CBSK: string, LFSK: string, KFSK: string }> = async ({ request, next, env }) => {
   const { cf } = request;
   const { city, regionCode, country } = cf;
   const ipaddr = request.headers.get("CF-Connecting-IP");
@@ -10,12 +10,19 @@ export const onRequestGet: PagesFunction<{ CBSK: string, LFSK: string }> = async
     .on("#regulations .tick:first-of-type", { element(el) { el.setInnerContent(`Regulatory compliance alerts for ${country} available. Access alerts ➞`); } })
     .onDocument({
       async end(end) { 
-        //const script = await clearbit(env.CBSK, ipaddr)
-        //  .then(streamFulfilled)
-        //  .catch(streamRejected);
-        const script = await leadfeeder(env.LFSK, ipaddr)
-          .then(streamFulfilledL)
+        const script = await clearbit(env.CBSK, ipaddr)
+          .then(streamFulfilled)
           .catch(streamRejected);
+        
+        //const script = await leadfeeder(env.LFSK, ipaddr)
+        //  .then(streamFulfilledL)
+        //  .catch(streamRejected);
+        //end.append(script, { html: true }); 
+        
+        //const script = await kickfire(env.KFSK, ipaddr)
+        //  .then(streamFulfilledK)
+        //  .catch(streamRejected);
+        
         end.append(script, { html: true }); 
       }
     })
@@ -24,40 +31,60 @@ export const onRequestGet: PagesFunction<{ CBSK: string, LFSK: string }> = async
   return transformed;
 };
 
-async function clearbit(key: string, ipaddr?: string): Promise<Reveal> {
+async function clearbit(key: string, ipaddr?: string): Promise<Clearbit> {
   if (ipaddr === null) return Promise.reject();
-  const endpoint = "https://reveal.clearbit.com/v1/companies/find?ip=";
-  return fetch(endpoint + ipaddr, { headers: { "Authorization": `Bearer ${key}` } }).then(response => {
-    return new Promise((resolve, reject) => response.ok ? resolve(response.json<Reveal>()) : reject(response.statusText))
+  const url = `https://reveal.clearbit.com/v1/companies/find?ip=${ipaddr}`;
+  return fetch(url, { headers: { "Authorization": `Bearer ${key}` } }).then(response => {
+    return new Promise((resolve, reject) => response.ok ? resolve(response.json<Clearbit>()) : reject(response.statusText))
   });
 }
 
-async function leadfeeder(key: string, ipaddr?: string): Promise<Company> {
+async function leadfeeder(key: string, ipaddr?: string): Promise<Leadfeeder> {
   if (ipaddr === null) return Promise.reject();
-  const endpoint = "https://api.lf-discover.com/companies?ip=";
-  return fetch(endpoint + ipaddr, { headers: { "X-API-KEY": key } }).then(response => {
-    return new Promise((resolve, reject) => response.ok ? resolve(response.json<Company>()) : reject(response.statusText))
+  const url = `https://api.lf-discover.com/companies?ip=${ipaddr}`;
+  return fetch(url, { headers: { "X-API-KEY": key } }).then(response => {
+    return new Promise((resolve, reject) => response.ok ? resolve(response.json<Leadfeeder>()) : reject(response.statusText))
   });
 }
 
-function streamFulfilled(reveal: Reveal): string {
+async function kickfire(key: string, ipaddr?: string): Promise<Kickfire> {
+  if (ipaddr === null) return Promise.reject();
+  const url = `https://api.kickfire.com/v3/company?ip=${ipaddr}&key=${key}`;
+  return fetch(url).then(response => {
+    return new Promise((resolve, reject) => response.ok ? resolve(response.json<Kickfire>()) : reject(response.statusText))
+  });
+}
+
+function streamFulfilled(clearbit: Clearbit): string {
   const rows = Object.entries({ 
-    Company: reveal.company.name,
-    Sector: reveal.company.category.sector,
-    Industry: reveal.company.category.industry,
-    "SIC Code": reveal.company.category.sicCode
+    Company: clearbit.company.name,
+    Sector: clearbit.company.category.sector,
+    Industry: clearbit.company.category.industry,
+    "SIC Code": clearbit.company.category.sicCode,
   }).map(value => `<tr><td style="color: #ef323d">${value[0]}</td><td style="font-weight: normal">${value[1]}</td></tr>`);
   return `<script>
     document.getElementById("p1").innerHTML = '<li class="tick hidden" style="padding: unset"><table style="width: 100%; border-spacing: unset";>${rows.join("")}</table></li>';
   </script>`;
 }
 
-function streamFulfilledL(company: Company): string {
+function streamFulfilledL(leadfeeder: Leadfeeder): string {
   const rows = Object.entries({ 
-    Company: company.company.name,
-    Domain: company.company.domain,
-    "SIC Code": company.company.industries.sic,
-    "Employee Count": `${company.company.employees_range.min} - ${company.company.employees_range.max}`
+    Company: leadfeeder.company.name,
+    Domain: leadfeeder.company.domain,
+    "SIC Code": leadfeeder.company.industries.sic,
+    "Employee Count": `${leadfeeder.company.employees_range.min} - ${leadfeeder.company.employees_range.max}`,
+  }).map(value => `<tr><td style="color: #ef323d">${value[0]}</td><td style="font-weight: normal">${value[1]}</td></tr>`);
+  return `<script>
+    document.getElementById("p1").innerHTML = '<li class="tick hidden" style="padding: unset"><table style="width: 100%; border-spacing: unset";>${rows.join("")}</table></li>';
+  </script>`;
+}
+
+function streamFulfilledK(kickfire: Kickfire): string {
+  const rows = Object.entries({ 
+    Company: kickfire.data[0].companyName,
+    Domain: kickfire.data[0].website,
+    "SIC Code": kickfire.data[0].sicCode,
+    "Employee Count": kickfire.data[0].employees,
   }).map(value => `<tr><td style="color: #ef323d">${value[0]}</td><td style="font-weight: normal">${value[1]}</td></tr>`);
   return `<script>
     document.getElementById("p1").innerHTML = '<li class="tick hidden" style="padding: unset"><table style="width: 100%; border-spacing: unset";>${rows.join("")}</table></li>';
@@ -68,7 +95,7 @@ function streamRejected(rejected: string): string {
   return `<script>document.getElementById("p1").innerHTML = '<li class="tick hidden" style="padding: unset">Unrecognized visitor (${rejected}).</li>'</script>`;
 }
 
-interface Reveal {
+interface Clearbit {
   ip: string;
   domain: string;
   type: string;
@@ -93,7 +120,7 @@ interface Reveal {
   };
 }
 
-interface Company {
+interface Leadfeeder {
   company: {
     revenue: {
       year: string;
@@ -112,4 +139,20 @@ interface Company {
       max: string;
     };
   };
+}
+
+interface Kickfire {
+  data: [
+    {
+      companyName: string;
+      tradeName: string;
+      website: string;
+      employees: string;
+      revenue: string;
+      sicGroup: string;
+      sicCode: string;
+      naicsGroup: string;
+      naicsCode: string;
+    }
+  ];
 }
