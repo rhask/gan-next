e0e00900338b6125d66037fc8d9c2e058ac947f6
rhask <brianjohncarbone@gmail.com>
2022-05-06 15:07:14 -0400

    ok

--- functions/index.ts
+++ functions/index.ts
@@ -10,19 +10,18 @@ export const onRequestGet: PagesFunction<{ CBSK: string }> = async ({ request, w
 
   const { readable, writable } = new TransformStream();
   waitUntil((async () => {
-    await scheduler.wait(1);
-    await response.body.pipeTo(writable, { preventClose: true });
-    const writer = writable.getWriter();
-    clearbit(env.CBSK, ipaddr)
-      .then(onfulfilled => {
-        const html = streamFulfilled(onfulfilled);
-        writer.write(new TextEncoder().encode(html));
-      })
-      .catch(onrejected => {
-        const html = streamRejected(onrejected);
-        writer.write(new TextEncoder().encode(html));
-      })
-      .finally(() => writer.close());
+    await response.body.pipeTo(writable);//, { preventClose: true });
+    //const writer = writable.getWriter();
+    //clearbit(env.CBSK, ipaddr)
+    //  .then(onfulfilled => {
+    //    const html = streamFulfilled(onfulfilled);
+    //    writer.write(new TextEncoder().encode(html));
+    //  })
+    //  .catch(onrejected => {
+    //    const html = streamRejected(onrejected);
+    //    writer.write(new TextEncoder().encode(html));
+    //  })
+    //  .finally(() => writer.close());
   })());
 
   return new Response(readable, response);
