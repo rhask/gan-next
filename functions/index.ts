75e639c8baf1ba154aa3ade5e9e1a60b8f3bc5ed
rhask <brianjohncarbone@gmail.com>
2022-05-06 16:50:28 -0400

    ok

--- functions/index.ts
+++ functions/index.ts
@@ -1,32 +1,27 @@
-export const onRequestGet: PagesFunction<{ CBSK: string }> = async ({ request, waitUntil, next, env }) => {
+export const onRequestGet: PagesFunction<{ CBSK: string, LFSK: string }> = async ({ request, next, env }) => {
   const { cf } = request;
   const { city, regionCode, country } = cf;
   const ipaddr = request.headers.get("CF-Connecting-IP");
   console.log(ipaddr);
   const response = await next();
 
-  //const transformed = new HTMLRewriter()
-  //  .on("span.country", { element(el) { el.setInnerContent(`${city}, ${regionCode}, ${country}`) } })
-  //  .on("#regulations .tick:first-of-type", { element(el) { el.setInnerContent(`Regulatory compliance alerts for ${country} available. Access alerts ➞`) } })
-  //  .transform(response);
+  const transformed = new HTMLRewriter()
+    .on("span.country", { element(el) { el.setInnerContent(`${city}, ${regionCode}, ${country}`); } })
+    .on("#regulations .tick:first-of-type", { element(el) { el.setInnerContent(`Regulatory compliance alerts for ${country} available. Access alerts ➞`); } })
+    .onDocument({
+      async end(end) { 
+        //const script = await clearbit(env.CBSK, ipaddr)
+        //  .then(streamFulfilled)
+        //  .catch(streamRejected);
+        const script = await leadfeeder(env.LFSK, ipaddr)
+          .then(streamFulfilledL)
+          .catch(streamRejected);
+        end.append(script, { html: true }); 
+      }
+    })
+    .transform(response);
 
-  const { readable, writable } = new TransformStream();
-  waitUntil((async () => {
-    await response.body.pipeTo(writable, { preventClose: true });
-    const writer = writable.getWriter();
-    clearbit(env.CBSK, ipaddr)
-      .then(onfulfilled => {
-        const html = streamFulfilled(onfulfilled);
-        writer.write(new TextEncoder().encode(html));
-      })
-      .catch(onrejected => {
-        const html = streamRejected(ipaddr);
-        writer.write(new TextEncoder().encode(html));
-      })
-      .finally(() => writer.close());
-  })());
-
-  return new Response(readable, response);
+  return transformed;
 };
 
 async function clearbit(key: string, ipaddr?: string): Promise<Reveal> {
@@ -37,12 +32,32 @@ async function clearbit(key: string, ipaddr?: string): Promise<Reveal> {
   });
 }
 
+async function leadfeeder(key: string, ipaddr?: string): Promise<Company> {
+  if (ipaddr === null) return Promise.reject();
+  const endpoint = "https://api.lf-discover.com/companies?ip=";
+  return fetch(endpoint + ipaddr, { headers: { "X-API-KEY": key } }).then(response => {
+    return new Promise((resolve, reject) => response.ok ? resolve(response.json<Company>()) : reject(response.statusText))
+  });
+}
+
 function streamFulfilled(reveal: Reveal): string {
   const rows = Object.entries({ 
     Company: reveal.company.name,
     Sector: reveal.company.category.sector,
     Industry: reveal.company.category.industry,
     "SIC Code": reveal.company.category.sicCode
+  }).map(value => `<tr><td style="color: #ef323d">${value[0]}</td><td style="font-weight: normal">${value[1]}</td></tr>`);
+  return `<script>
+    document.getElementById("p1").innerHTML = '<li class="tick hidden" style="padding: unset"><table style="width: 100%; border-spacing: unset";>${rows.join("")}</table></li>';
+  </script>`;
+}
+
+function streamFulfilledL(company: Company): string {
+  const rows = Object.entries({ 
+    Company: company.company.name,
+    Domain: company.company.domain,
+    "SIC Code": company.company.industries.sic,
+    "Employee Count": `${company.company.employees_range.min} - ${company.company.employees_range.max}`
   }).map(value => `<tr><td style="color: #ef323d">${value[0]}</td><td style="font-weight: normal">${value[1]}</td></tr>`);
   return `<script>
     document.getElementById("p1").innerHTML = '<li class="tick hidden" style="padding: unset"><table style="width: 100%; border-spacing: unset";>${rows.join("")}</table></li>';
@@ -74,6 +89,27 @@ interface Reveal {
       employees: number;
       employeesRange: string;
       estimatedAnnualRevenue: string;
+    };
+  };
+}
+
+interface Company {
+  company: {
+    revenue: {
+      year: string;
+      amount: string;
+    };
+    domain: string;
+    logo_url: string;
+    industries: {
+      name: string;
+      sic: [string];
+      naics: [string];
+    };
+    name: string;
+    employees_range: {
+      min: string;
+      max: string;
     };
   };
 }
