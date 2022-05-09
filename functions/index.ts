3fe45a37829d81d5efda8b72e11f5b8b1061a67f
rhask <brianjohncarbone@gmail.com>
2022-05-09 17:03:54 -0400

    ok

--- functions/index.ts
+++ functions/index.ts
@@ -1,7 +1,7 @@
 export const onRequestGet: PagesFunction<{ CBSK: string, LFSK: string, KFSK: string }> = async ({ request, next, env }) => {
   const { cf } = request;
   const { city, regionCode, country } = { ...cf };
-  const ipaddr = request.headers.get("CF-Connecting-IP");
+  const ipaddr = "74.64.207.161" || request.headers.get("CF-Connecting-IP");
   console.log(ipaddr);
   const response = await next();
 
